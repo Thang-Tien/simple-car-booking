1d7c75f3e1914502ab566ac8edaeba9c73121973
Thang Tien_21020403 <cao.hl.thang@gmail.com>
2024-09-28 16:27:20 +0700

    init router

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+	log.Fatal(http.ListenAndServe(":8000", r))
+}
